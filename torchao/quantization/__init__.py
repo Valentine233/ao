3c89a14b62d006ed8ac3e40db9a64d5f61468273
Valentine233 <xuan.liao@intel.com>
2025-02-26 00:52:55 -0500

    update

--- torchao/quantization/__init__.py
+++ torchao/quantization/__init__.py
@@ -96,9 +96,6 @@ from .smoothquant import (
     smooth_fq_linear_to_inference,
     swap_linear_with_smooth_fq_linear,
 )
-from .sfdp_int8_fx_pass import (
-    _sfdp_int8_init,
-)
 from .subclass import *  # noqa: F403
 from .transform_module import register_quantize_module_handler
 from .unified import Quantizer, TwoStepQuantizer
@@ -191,5 +188,4 @@ __all__ = [
     "WeightOnlyInt8QuantLinear",
     "TwoStepQuantizer",
     "Quantizer",
-    "_sfdp_int8_init",
 ]
